76e63945eddcf7624e4b0057fd470d083a3262ef
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2023-03-16 20:30:51 +0530

    Added new file catchAsync.js for clean code writing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const ejsMate = require('ejs-mate');
-const Campground = require('./models/campground')
-const methodOverride = require('method-override')
+const Campground = require('./models/campground');
+const methodOverride = require('method-override');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
@@ -25,55 +27,57 @@ app.get('/',(req ,res)=>{
     res.render('home')
 })
 
-app.get('/makecampground',async (req ,res)=>{
+app.get('/makecampground',catchAsync(async (req ,res)=>{
     const camp = new Campground({title:'My Backyard' ,description:'This is a campground' ,price:'20'})
     await camp.save();
     res.send(camp)
-})
+}));
 
-app.get('/campgrounds' ,async(req ,res)=>{
+app.get('/campgrounds' ,catchAsync(async(req ,res)=>{
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index' , {campgrounds});
-})
+}))
 //Adding new campground
-app.get('/campgrounds/new',async (req,res)=>{
+app.get('/campgrounds/new',catchAsync(async (req,res)=>{
     res.render('campgrounds/new');
-})
-app.post('/campgrounds',async (req,res,next)=>{
-    try{
+}))
+app.post('/campgrounds',catchAsync(async (req,res,next)=>{
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`);
-    }catch(e){
-        next(e);
-    }
-})
+}))
 
 //showing the camp details
-app.get('/campgrounds/:id',async(req,res)=>{
+app.get('/campgrounds/:id', catchAsync(async(req,res)=>{
     const campgrounds = await Campground.findById(req.params.id);
     res.render('campgrounds/show' , {campgrounds});
-})
+}))
 //editing
-app.get('/campgrounds/:id/edit',async(req,res)=>{
+app.get('/campgrounds/:id/edit', catchAsync(async(req,res)=>{
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit' ,{campground});
-})
+}))
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
-});
+}));
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+//Error handeling of express
+app.all('*',(req,res,next)=>{
+    next(new ExpressError('Page not Found' ,404))
 })
 
 app.use((err,req,res,next)=>{
-    res.send("Their's an error!")
+    const {statusCode = 500 , message = 'Something went worng'} = err;
+    res.status(statusCode).send(message);
 })
 
 
