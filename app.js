0ea3ada447a000a166178f4ebee73b114db506e2
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2023-03-16 22:54:09 +0530

    Change in app.js ,Server side validation with JOI

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
+const Joi = require('joi');
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
@@ -42,7 +43,24 @@ app.get('/campgrounds/new',catchAsync(async (req,res)=>{
     res.render('campgrounds/new');
 }))
 app.post('/campgrounds',catchAsync(async (req,res,next)=>{
-    if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
+    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
+    // using JOI for validation of data 
+    const campgroundSchema = Joi.object({
+        campground:Joi.object({
+            title:Joi.string().required(),
+            price:Joi.number().required().min(0),
+            image:Joi.string().required(),
+            location:Joi.string().required(),
+            description:Joi.string().required()
+        }).required()
+    })    
+    const { error } = campgroundSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
+    console.log(result);
+
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`);
