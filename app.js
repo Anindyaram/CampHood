34b07f71396b97ddf3b1b939410f1e78accc8a57
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2023-03-19 17:19:34 +0530

    Seperate file for campground and review routes

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const path = require('path');
-const ejsMate = require('ejs-mate');
-const Campground = require('./models/campground');
-const methodOverride = require('method-override');
-const catchAsync = require('./utils/catchAsync');
-const ExpressError = require('./utils/ExpressError');
-const { campgroundSchema ,reviewSchema} = require('./schemas.js');
-const Review = require('./models/review')
-
-const campgrounds = require('./routes/campgrounds')
-
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
+const ExpressError = require('./utils/ExpressError');
+const reviews = require('./routes/reviews')
+const campgrounds = require('./routes/campgrounds')
 const { validate } = require('./models/campground');
+
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 const db = mongoose.connection;
@@ -28,51 +24,13 @@ app.set('view engine' ,'ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'))
 
-//Server side validation with the help of joi 
-const validateCampground = (req,res,next)=>{
-        const { error } = campgroundSchema.validate(req.body);
-        if(error){
-            const msg = error.details.map(el => el.message).join(',');
-            throw new ExpressError(msg, 400);
-        }else{
-            next();
-        }
-        // console.log(result);
-}
-
-const validateReview = (req,res,next)=>{
-    const {error} = reviewSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    }else{
-        next();
-    }
-}
-//campground Routes
+//campground and review Router
 app.use('/campgrounds' , campgrounds);
+app.use('/campgrounds/:id/reviews' , reviews);
 
 app.get('/',(req ,res)=>{
     res.render('home')
 })
-
-
-//Creating review system
-app.post('/campgrounds/:id/reviews' ,validateReview, catchAsync(async(req,res)=>{
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-app.delete('/campgrounds/:id/reviews/:reviewId' , catchAsync(async(req,res)=>{
-    const {id,reviewId} = req.params;
-    await Campground.findByIdAndUpdate(id , {$pull:{reviews:reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}))
 
 //Error handeling of express
 app.all('*',(req,res,next)=>{
@@ -84,7 +42,6 @@ app.use((err,req,res,next)=>{
     if(!err.message) err.message = 'Oh no, Something went wrong!';
     res.status(statusCode).render('error' ,{ err });
 })
-
 
 app.listen(3000,()=>{
     console.log('Connected to port 3000')
