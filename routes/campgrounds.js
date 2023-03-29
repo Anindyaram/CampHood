c857c8139e9b97f5a374e824facf57d4b7e7dd6e
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2023-03-30 00:18:29 +0530

    Added cloudinary for image upload

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,16 +4,19 @@ const campgrounds = require('../controllers/campground')
 const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const { isLoggedIn ,isAuthor ,validateCampground} = require('../middleware');
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 router.route('/')
     .get(catchAsync(campgrounds.index))    //Added campground.index by using a model
-    .post(validateCampground ,catchAsync(campgrounds.createNewCampground))//creating new campground
+    .post(isLoggedIn, upload.array('image'),validateCampground ,catchAsync(campgrounds.createNewCampground))//creating new campground
 
 router.get('/new', isLoggedIn,catchAsync(campgrounds.renderNewForm))//Adding new campground
 
 router.route('/:id')
     .get(catchAsync(campgrounds.showCampground)) //showing the camp details
-    .put(isLoggedIn ,isAuthor ,validateCampground , catchAsync(campgrounds.updateCampground))//Updating campground
+    .put(isLoggedIn ,isAuthor ,upload.array('image'),validateCampground , catchAsync(campgrounds.updateCampground))//Updating campground
     .delete(isLoggedIn,isAuthor, catchAsync(campgrounds.deleteCampground)) //deleting campground
 
 router.get('/:id/edit' ,isLoggedIn ,isAuthor , catchAsync(campgrounds.renderEditForm))//editing
