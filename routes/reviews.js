a09926db9daab2a99eea2f589817b7bb865f0ff3
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2023-03-29 18:06:07 +0530

    Fixed delete button showing

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,7 +4,7 @@ const router = express.Router({ mergeParams:true });//need to be true for accesi
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 const catchAsync = require('../utils/catchAsync');
-const { validateReview ,isLoggedIn}= require('../middleware')
+const { validateReview ,isLoggedIn ,isReviewAuthor}= require('../middleware')
 
 //Creating review system
 router.post('/' ,isLoggedIn ,validateReview , catchAsync(async(req,res)=>{
@@ -18,7 +18,7 @@ router.post('/' ,isLoggedIn ,validateReview , catchAsync(async(req,res)=>{
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
-router.delete('/:reviewId' , catchAsync(async(req,res)=>{
+router.delete('/:reviewId' ,isLoggedIn, isReviewAuthor, catchAsync(async(req,res)=>{
     const {id,reviewId} = req.params;
     await Campground.findByIdAndUpdate(id , {$pull:{reviews:reviewId}});
     await Review.findByIdAndDelete(reviewId);
