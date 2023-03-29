78b5de39d34019a00405b76827a134254002ed3e
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2023-03-29 22:23:11 +0530

    Created users controller

--- routes/users.js
+++ routes/users.js
@@ -3,45 +3,17 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 const passport = require('passport');
+const users = require('../controllers/users');
 
-router.get('/register' , (req,res)=>{
-    res.render('users/register');
-})
-
-router.post('/register' ,catchAsync(async(req ,res)=>{
-    try{
-    const{ email , username , password} = req.body;
-    const user = new User({email , username});
-    const registeredUser = await User.register(user , password);
-    req.login(registeredUser, err=>{
-        if(err) return next(err);
-        req.flash('success','Welcome to CampHood!!');
-        res.redirect('/campgrounds')
-    })
-    }catch(e){
-        req.flash('error' ,e.message);
-        res.redirect('register');
-    }
-}))
-
+//Redirecting to register form 
+router.get('/register' , users.renderRegister);
+//registering new user
+router.post('/register' ,catchAsync(users.register))
 //login 
-router.get('/login' ,(req,res)=>{
-    res.render('users/login');
-})
+router.get('/login' ,users.renderLogin)
 //passport.authenticate() is a middleware by passport which  can be used for various login type
-router.post('/login' ,passport.authenticate('local' ,{failureFlash:true , failureRedirect:'/login'}),(req,res)=>{
-    //If we get here it means user is authenticated successfully
-    req.flash('success' ,'Welcome Back!!');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-
-})
-
-router.get('/logout',(req,res)=>{
-    req.logout();
-    req.flash('success','GoodBye!!');
-    res.redirect('/campgrounds');
-})
+router.post('/login' ,passport.authenticate('local' ,{failureFlash:true , failureRedirect:'/login'}),users.login)
+//logout from the website
+router.get('/logout',users.logout)
 
 module.exports = router;
